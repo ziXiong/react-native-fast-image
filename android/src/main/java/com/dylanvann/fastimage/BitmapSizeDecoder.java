bb05adcd02ae2e58db10d74104e2e3433222aa61
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-02-14 11:33:25 +0100

    Update patch

--- android/src/main/java/com/dylanvann/fastimage/BitmapSizeDecoder.java
+++ android/src/main/java/com/dylanvann/fastimage/BitmapSizeDecoder.java
@@ -4,6 +4,7 @@ import android.graphics.BitmapFactory;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import com.bumptech.glide.load.resource.SimpleResource;
 
 import com.bumptech.glide.load.Options;
 import com.bumptech.glide.load.ResourceDecoder;
@@ -26,6 +27,18 @@ public class BitmapSizeDecoder implements ResourceDecoder<InputStream, BitmapFac
         BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
         bitmapOptions.inJustDecodeBounds = true;
         BitmapFactory.decodeStream(source, null, bitmapOptions);
+
+        // BitmapFactory#decodeStream leaves stream's position where ever it was after reading the encoded data
+        // https://developer.android.com/reference/android/graphics/BitmapFactory#decodeStream(java.io.InputStream)
+        // so we need to rewind the stream to be able to read image header with exif values
+        source.reset();
+
+        int orientation = new ExifInterface(source).getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90 || orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            int tmpWidth = bitmapOptions.outWidth;
+            bitmapOptions.outWidth = bitmapOptions.outHeight;
+            bitmapOptions.outHeight = tmpWidth;
+        }
         return new SimpleResource(bitmapOptions);
     }
 }
